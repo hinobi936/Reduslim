342b3012a5fdb7d7f45027d5f04765ac5e86cee8
hinobi <56470494+hinobi936@users.noreply.github.com>
2021-06-11 15:24:07 +0500

    Add files via upload

--- js/select.js
+++ js/select.js
@@ -1,36 +1,29 @@
-
 var ddData = [
-    {
-        text: "RU",
-        value: 1,
-        selected: true,
-        imageSrc: "/img/rus.jpg"
-    },
-    {
-        text: "UA",
-        value: 2,
-        selected: false,
-        imageSrc: "/img/usa.jpg"
-    },
-    {
-        text: "KZ",
-        value: 3,
-        selected: true,
-        imageSrc: "/img/kaz.jpg"
-    },
-    {
-        text: "UK",
-        value: 4,
-        selected: false,
-        imageSrc: "/img/ukr.jpg"
-    }
+  {
+    text: "RU",
+    value: 1,
+    selected: true,
+    imageSrc: "/img/rus.jpg",
+  },
+  {
+    text: "UA",
+    value: 2,
+    selected: false,
+    imageSrc: "/img/usa.jpg",
+  },
+  {
+    text: "UK",
+    value: 4,
+    selected: false,
+    imageSrc: "/img/ukr.jpg",
+  },
 ];
 
-$('#myDropdown').ddslick({
-    data:ddData,
-    width: 100,
-    imagePosition:"right",
-    onSelected: function(selectedData){
-        //callback function: do something with selectedData;
-    }   
+$("#myDropdown").ddslick({
+  data: ddData,
+  width: 100,
+  imagePosition: "right",
+  onSelected: function (selectedData) {
+    //callback function: do something with selectedData;
+  },
 });
